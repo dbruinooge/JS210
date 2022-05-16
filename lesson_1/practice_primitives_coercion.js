cc8a8d93821c3d4bb226fd6624de2e2b3edfb495
Derek Bruinooge <derek.bruinooge@gmail.com>
2022-05-16 12:28:41 +0000

    Add lesson 1 practice problems

---
+++ lesson_1/practice_primitives_coercion.js
@@ -0,0 +1,21 @@
+let x = parseInt('13', 10);
+let y = 9;
+
+console.log(x + y);
+
+x = '13';
+y = 9;
+
+console.log(x * y);
+
+let npa = String(212);
+let nxx = String(555);
+let num = String(1212);
+
+console.log(npa + nxx + num);
+
+let bool = true;
+let arr = [1, 2, 3];
+
+console.log(bool.toString());
+console.log(arr.toString());
